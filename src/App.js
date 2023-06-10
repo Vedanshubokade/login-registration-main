755e2696a06b231db52e35f2471c7840ddbbc2c3
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-06-10 14:53:54 +0530

    Added user and home commonnts fro route /user ,/admin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, 
+  // Link 
+} from "react-router-dom";
 
 import Login from "./components/login_component";
 import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
-import ImageUpload from "./components/imageUpload.";
+import UserHome from "./components/userHome";
+import AdminHome from "./components/adminHome";
+// import ImageUpload from "./components/imageUpload.";
 
 function App() {
-  const isLoggedIn = window.localStorage.getItem("loggedIn");
+  // const isLoggedIn = window.localStorage.getItem("loggedIn");
   return (
     <Router>
       <div className="App">
@@ -17,11 +21,15 @@ function App() {
           <Route
             exact
             path="/"
-            element={isLoggedIn == "true" ? <UserDetails /> : <Login />}
+            element={<Login/>}
+            // element={isLoggedIn === "true" ? <UserDetails /> : <Login />}
+            
           />
           <Route path="/sign-in" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/userDetails" element={<UserDetails />} />
+          <Route path="/user" element={<UserHome />} />
+          <Route path="/admin" element={<AdminHome />} />
         </Routes>
         {/* <ImageUpload/> */}
       </div>
